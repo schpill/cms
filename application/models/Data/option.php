737bb1eb77a4328bfff8b90df619d1378770ac8a
Gerald Plusquellec Qc <gplusquellec@gmail.com>
2014-01-20 09:42:25 -0500

    ake

--- application/models/Data/option.php
+++ application/models/Data/option.php
@@ -2,7 +2,7 @@
     return array(
         'fields' => array(
             'name'      => array('label' => 'Nom'),
-            'value'     => array('label' => 'Valeur'),
+            'value'     => array('label' => 'Valeur', 'type' => 'textarea'),
         ),
         'settings' => array(
             'orderList'             => 'name',
